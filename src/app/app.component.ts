4973cd9abe6ee90650c9d8431820781c69b75f34
maksim-luzin <333maximax333@gmail.com>
2022-09-03 06:57:16 +0300

    Adding a Main Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Certificate } from 'src/app/models';
+import { CertificateService } from './services/certificate.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'test-for-taxer';
+export class AppComponent implements OnInit {
+  certificateList!: Certificate[];
+  isAddCertificate!: boolean;
+
+  constructor(private certificateService: CertificateService) {}
+
+  ngOnInit() {
+    this.certificateList =
+      this.certificateService.getCertificateList() as Certificate[];
+  }
+
+  ngAfterContentChecked() {
+    this.isAddCertificate = this.certificateService.isAddCertificate;
+  }
+
+  toggleIsAddCertificate() {
+    this.certificateService.toggleIsAddCertificate();
+  }
+
+  getError() {
+    return this.certificateService.getError();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserCommonNameComponent } from './components/user-common-name/user-common-name.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CertificateService } from './services/certificate.service';
+import { CertificateUploadComponent } from './components/certificate-upload/certificate-upload.component';
+import { DropZoneDirective } from './directive/drop-zone-directive';
+import { LocalStorageService } from './services/local-storage.service';
+import { CertificateFileService } from './services/certificate-file.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCommonNameComponent,
+    UserInfoComponent,
+    CertificateUploadComponent,
+    DropZoneDirective
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, BrowserAnimationsModule],
+  providers: [CertificateService, LocalStorageService, CertificateFileService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
